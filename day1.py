4f0ca6c65b9e9f16fe7139dc668c18930db2c51f
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-06-10 13:50:23 -0700

    DSA-1

---
+++ day1.py
@@ -0,0 +1,17 @@
+"""Single Loop"""
+# for i in range(5):
+#     print(i)
+
+
+"""Nested Loop"""
+# for i in range(2):
+#     for j in range(2):
+#         print(i, j)
+
+
+"""Count Loop Executions"""
+# count = 0
+# for i in range(5):
+#     count += 1
+#     print(count)
+ 
