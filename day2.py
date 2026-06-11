957dddfccc8ec45f1adeebcea967be9383fd8b97
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-06-11 09:52:31 -0700

    DSA-1

---
+++ day2.py
@@ -0,0 +1,36 @@
+"""Traverse an Array"""
+# arr = [10, 20, 30, 40]
+# for num in arr:
+#     print(num)
+
+
+"""Indexing"""
+# arr = [10, 20, 30]
+# print(arr[0])
+# print(arr[1])
+
+
+"""Finding Largest Number"""        
+# arr = [10, 20, 30, 40, 50]
+# largest = arr[0]
+# for num in arr:
+#     if num > largest:
+#         largest = num
+# print(largest)                 
+
+
+"""Finding Smallest Number"""
+# arr = [10, 20, 30, 40, 50]
+# smallest = arr[0]
+# for num in arr:
+#     if num < smallest:
+#         smallest = num
+# print(smallest)        
+
+
+"""Sum of Array Elements"""
+# numb = [10, 20, 30, 40]
+# total = 0
+# for num in numb:
+#     total += num
+# print(total)    
